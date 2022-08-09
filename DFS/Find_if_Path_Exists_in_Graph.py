503e78518d50234c860c986e6046f6c7ac3db788
harika-pendli <hpendli@umd.edu>
2022-08-08 21:56:27 -0400

    changed the file names and added links to the problems

---
+++ DFS/Find_if_Path_Exists_in_Graph.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def validPath(self, n, edges, source, destination):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type source: int
+        :type destination: int
+        :rtype: bool
+        """
+        adjacency_list={k:[] for  k in range(n)}
+        for edge in edges:
+            adjacency_list[edge[0]].append(edge[1])
+            adjacency_list[edge[1]].append(edge[0])
+            
+        
+        def dfs(adjacency_list,source,destination):
+            marked= [False]*n
+            flag=False
+            stack=[source]
+            while len(stack)>0:
+                v=stack.pop()
+                if v==destination:
+                    flag=True
+                
+                if not marked[v]:
+                    marked[v]=True
+                    for w in adjacency_list[v]:
+                        if not marked[w]:
+                            stack.append(w)
+            return flag
+        
+        
+        return dfs(adjacency_list,source,destination)
+                            
+                
+            
+            
+            
+                
+        
